38f93412424856fc2a20577352d91237eca5a00e
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-01-09 20:28:37 +0530

    send photo and video feature added in bot

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import time
 import requests
 
 
-
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 # ADMIN_USER = "WinbuzzPromoter".strip()  # username of the admin user
@@ -53,8 +52,9 @@ def advertise(update, context):
         context.user_data['advertising_user'] = user
         context.user_data['advertising_message'] = None
 
-        # Use the MessageHandler to register the advertising message handler
-        message_handler = MessageHandler(Filters.text & ~Filters.command, advertising_message_handler)
+        # Add a new handler to capture the admin's advertising message
+        message_handler = MessageHandler(
+            Filters.text | Filters.photo | Filters.video & ~Filters.command, advertising_message_handler)
         context.dispatcher.add_handler(message_handler)
 
     else:
@@ -70,16 +70,15 @@ def advertising_message_handler(update, context):
     # Check if the message sender is the admin who initiated the advertising
     if user.username == advertising_user.username:
         # Save the advertising message
-        context.user_data['advertising_message'] = update.message.text
+        # context.user_data['advertising_message'] = update.message.text
+        context.user_data['advertising_message'] = update.message
 
         advertising_message = context.user_data['advertising_message']
         print(f"Advertising Message: {advertising_message}")
 
         # Inform the admin that the advertising message has been received
         update.message.reply_text(
-            f"Advertising message received. It will be posted in the channels.\n\n"
-            f"Advertising Message:\n {advertising_message}"
-        )
+            f"Advertising message received. It will be posted in the channels.")
 
         # Send the advertising message to all channels where you are admin
         send_advertising_message_to_all_channels(context)
@@ -93,24 +92,46 @@ def send_advertising_message_to_all_channels(context):
     advertising_message = context.user_data['advertising_message']
 
     if CHAT_ID:
-        # Convert the string of chat IDs to a list of integers
         chat_ids = [int(chat_id) for chat_id in CHAT_ID.split(',')]
 
         for chat_id in chat_ids:
-            url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-            params = {
-                'chat_id': chat_id,
-                'text': advertising_message
-            }
-            response = requests.post(url, data=params)
-            print(response.json())
+            # Check the type of media and send accordingly
+            if advertising_message.text:
+                # Text message
+                text_params = {
+                    'chat_id': chat_id,
+                    'text': advertising_message.text
+                }
+                response = requests.post(
+                    f'https://api.telegram.org/bot{TOKEN}/sendMessage', data=text_params)
+                print(response.json())
+            elif advertising_message.photo:
+                # Photo
+                photo_params = {
+                    'chat_id': chat_id,
+                    'photo': advertising_message.photo[-1].file_id,
+                    'caption': advertising_message.caption if advertising_message.caption else ""
+                }
+                response = requests.post(
+                    f'https://api.telegram.org/bot{TOKEN}/sendPhoto', data=photo_params)
+                print(response.json())
+            elif advertising_message.video:
+                # Video
+                video_params = {
+                    'chat_id': chat_id,
+                    'video': advertising_message.video.file_id,
+                    'caption': advertising_message.caption if advertising_message.caption else ""
+                }
+                response = requests.post(
+                    f'https://api.telegram.org/bot{TOKEN}/sendVideo', data=video_params)
+                print(response.json())
 
 
 # Schedule the advertisement at 12 PM every day
-schedule.every().day.at("12:00").do(advertise)
+# schedule.every().day.at("12:00").do(advertise)
 
 # Schedule the advertisement at 6 PM every day
-schedule.every().day.at("18:00").do(advertise)
+# schedule.every().day.at("18:00").do(advertise)
 
 updater = telegram.ext.Updater(TOKEN, use_context=True)
 dispatch = updater.dispatcher
